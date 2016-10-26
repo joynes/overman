a7957e036c88ea38bc3788f00f1ee0cd7a488143
johannes <johannes@spotify.com>
2016-10-26 14:56:07 +0200

    Test without timeouts

--- test/util/never_ending_program_that_may_fork_subprocess.js
+++ test/util/never_ending_program_that_may_fork_subprocess.js
@@ -1,5 +1,12 @@
 if (process.argv[2] === 'fork') {
-  require('child_process').fork(__dirname + '/../util/never_ending_program_that_may_fork_subprocess.js', ['empty']);
+  proc = require('child_process').fork(__dirname + '/../util/never_ending_program_that_may_fork_subprocess.js', ['empty']);
+  // listen to when forking is done and send it to the parent
+  proc.on('message', function(data) {
+    process.send(data);
+  });
+} else {
+  // Send a message that forking is done
+  process.send({ state: 'forked' });
 }
 
 setInterval(function() {}, 1000);
